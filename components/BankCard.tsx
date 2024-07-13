75757ea3b1cf9d8725e20b6ab6df902defc137b6
ares <notroot@pop-os.localdomain>
2024-07-13 16:18:34 -0700

    implement transaction history, banks and payment transfer pages. Add new components for the new pages and update server actions

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,6 +2,7 @@ import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
@@ -10,7 +11,10 @@ const BankCard = ({
 }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card min-w-[325px]"
+      >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">{userName}</h1>
@@ -23,7 +27,7 @@ const BankCard = ({
               <h1 className="text-12 font-semibold text-white">{userName}</h1>
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
-            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+            <p className="text-14 font-semibold tracking-[0.5px] text-white">
               ●●●● ●●●● ●●●● <span className="text-16">{account.mask}</span>
             </p>
           </article>
@@ -46,6 +50,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.sharableId} />}
     </div>
   );
 };
--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -11,6 +11,7 @@ import {
   createLinkToken,
   exchangePublicToken,
 } from "@/lib/actions/user.actions";
+import Image from "next/image";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter();
@@ -47,13 +48,39 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   return (
     <>
       {variant === "primary" ? (
-        <Button onClick={() => open()} disabled={!ready}>
+        <Button
+          className="plaidlink-primary"
+          onClick={() => open()}
+          disabled={!ready}
+        >
           Connect Bank
         </Button>
       ) : variant === "ghost" ? (
-        <Button>Connect Bank</Button>
+        <Button
+          onClick={() => open()}
+          variant="ghost"
+          className="plaidlink-ghost"
+        >
+          <Image
+            src="/icons/connect-bank.svg"
+            alt="connect bank"
+            width={24}
+            height={24}
+          />
+          <p className="hiddenl text-[16px] font-semibold text-black-2 xl:block">
+            Connect Bank
+          </p>
+        </Button>
       ) : (
-        <Button>Connect Bank</Button>
+        <Button onClick={() => open()} className="plaidlink-default">
+          <Image
+            src="/icons/connect-bank.svg"
+            alt="connect bank"
+            width={24}
+            height={24}
+          />
+          <p className="text-[16px] font-semibold text-black-2">Connect Bank</p>
+        </Button>
       )}
     </>
   );
